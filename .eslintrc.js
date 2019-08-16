35a063dac0eb054aee50cd2372940d362cf50d50
dov-yih <yidafu90@qq.com>
2019-08-16 22:33:11 +0800

    init project by create-react-add and add eslint config. Ok let's make this nice beer

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    parser: '@typescript-eslint/parser',
+    "extends": [
+        "airbnb",
+        "airbnb-typescript",
+    ],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        '@typescript-eslint',
+    ],
+    "rules": {
+    }
+};
---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.css';
+
+const App: React.FC = () => (
+  <div className="App">
+    <h1>Let&apos;s Drink A Cup Of Nice Beer!</h1>
+  </div>
+);
+
+export default App;
