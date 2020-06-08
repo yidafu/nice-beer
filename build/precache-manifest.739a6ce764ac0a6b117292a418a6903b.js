73f32a604772cb21fc1970d6a16771707bcedbe0
yidafu <tiaozuwushi@qq.com>
2020-06-08 12:53:39 +0800

    build: update build/ dir

---
+++ build/precache-manifest.739a6ce764ac0a6b117292a418a6903b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "271fef023320e7469767f9db4edf13f6",
+    "url": "https://cdn.jsdelivr.net/gh/yidafu/nice-beer@dist/build/index.html"
+  },
+  {
+    "revision": "4eb488bec4876da97542",
+    "url": "https://cdn.jsdelivr.net/gh/yidafu/nice-beer@dist/build/static/css/2.1c7ffa40.chunk.css"
+  },
+  {
+    "revision": "2584c2e628371a12d18a",
+    "url": "https://cdn.jsdelivr.net/gh/yidafu/nice-beer@dist/build/static/css/main.d177da67.chunk.css"
+  },
+  {
+    "revision": "4eb488bec4876da97542",
+    "url": "https://cdn.jsdelivr.net/gh/yidafu/nice-beer@dist/build/static/js/2.85564489.chunk.js"
+  },
+  {
+    "revision": "0b08544f54c9906705be39c03b7e79ad",
+    "url": "https://cdn.jsdelivr.net/gh/yidafu/nice-beer@dist/build/static/js/2.85564489.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2584c2e628371a12d18a",
+    "url": "https://cdn.jsdelivr.net/gh/yidafu/nice-beer@dist/build/static/js/main.0225c12e.chunk.js"
+  },
+  {
+    "revision": "2b5aacf160b52002a969",
+    "url": "https://cdn.jsdelivr.net/gh/yidafu/nice-beer@dist/build/static/js/runtime-main.8a442b3f.js"
+  }
+]);
