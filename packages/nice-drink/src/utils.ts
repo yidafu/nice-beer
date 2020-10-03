04aef3fdf857e0dfc941e1b621fb4eb17fd3e223
yidafu <tiaozuwushi@qq.com>
2020-10-03 16:33:48 +0800

    fix: TypeError: a.default.existsSync is not a function

--- packages/nice-drink/src/utils.ts
+++ packages/nice-drink/src/utils.ts
@@ -1,6 +1,9 @@
 import chalk from 'chalk';
 import dayjs from 'dayjs';
+import { execSync } from 'child_process';
+import debug from 'debug';
 
+const log = debug('nice-drink:utils');
 
 export function hasFrontMatter(content: string) {
   return !content.trim().indexOf('---');
@@ -21,4 +24,25 @@ export function formatDate(dataStr: Date) {
 
 export function isRoot(filepath: string) {
   return filepath === '/' || /^[a-zA-Z]:\\$/.test(filepath);
+}
+
+export function idNodeEnv(): boolean {
+  return false;
+}
+
+export function getUpdatedAt(filePath: string): Date {
+  const command = `git log --pretty="format:%ci" ${filePath} | head -1`;
+  log(`getCreatedAt: ${command}`);
+  const dateStr = execSync(`git log --pretty="format:%ci" ${filePath} | head -1`).toString();
+  log(`create string: ${dateStr}`);
+  return new Date(dateStr);
+}
+
+
+export function getCreatedAt(filePath: string): Date {
+  const command = `git log --reverse --pretty="format:%ci" ${filePath} | head -1`;
+  log(`getUpdatedAt: ${command}`);
+  const dateStr = execSync(command).toString();
+  log(`update string: ${dateStr}`);
+  return new Date(dateStr);
 }
