d0426e0c1112e0ace10fd2aa83b623a9167dea29
yidafu <tiaozuwushi@qq.com>
2020-11-18 23:07:09 +0800

    feat: add function fetchContent type define

---
+++ packages/nice-drink/src/fetch/fetchAll.ts
@@ -0,0 +1,11 @@
+import { IPlainPost } from "../MarkdownPost";
+
+/**
+ * 暴露给第三方使用的，用户获取文章目录
+ *
+ * @export
+ * @param {string} url
+ */
+export function fetchContent(url: string): Promise<IPlainPost[]> {
+
+}
